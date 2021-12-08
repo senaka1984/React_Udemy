2b6933cb45e1d49a21b50cb32e801d8b8f7f9a89
senaka1984 <senakaelladeniya@gmail.com>
2021-12-08 23:20:17 +0800

    4 state

---
+++ 4.State/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/Expenses/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
+
+  const expensesdata = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Lets get started"),
+  //   React.createElement(Expenses, { expenses: expensesdata })
+  // );
+
+
+  return (
+    <div>
+      <h2>lets get started</h2>
+      <Expenses expenses={expensesdata} />
+    </div>
+
+  );
+}
+
+export default App;
