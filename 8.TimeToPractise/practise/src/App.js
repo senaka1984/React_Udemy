5cb6be18844a28a19ec83dfeb45557bb2e6509d7
senaka1984 <senakaelladeniya@gmail.com>
2021-12-14 13:04:10 +0800

    commit

---
+++ 8.TimeToPractise/practise/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+import AddUser from './Components/Users/AddUser'
+import UserList from './Components/Users/UserList';
+
+function App() {
+
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
