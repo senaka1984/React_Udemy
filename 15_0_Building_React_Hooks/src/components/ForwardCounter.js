29d9ad495fba358ae7489000cb3bbd99a20b5765
senaka1984 <senakaelladeniya@gmail.com>
2021-12-18 20:12:36 +0800

    commit

---
+++ 15_0_Building_React_Hooks/src/components/ForwardCounter.js
@@ -0,0 +1,13 @@
+
+import Card from './Card';
+import useCounter from '../hooks/use-counter';
+
+const ForwardCounter = () => {
+
+  const counter = useCounter(true);
+  return <Card>{counter}</Card>;
+
+};
+
+export default ForwardCounter;
+w
