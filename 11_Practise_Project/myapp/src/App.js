38fb472025f38f11440bdf6c7453e1af19a672e1
senaka1984 <senakaelladeniya@gmail.com>
2021-12-18 11:52:09 +0800

    complete food app

--- 11_Practise_Project/myapp/src/App.js
+++ 11_Practise_Project/myapp/src/App.js
@@ -1,15 +1,29 @@
-import { Fragment } from 'react'
+import { useState } from 'react'
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CardProvider from './store/CartProvider'
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment >
-      <Header />
+    <CardProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CardProvider>
   );
 }
 
--- 11_Practise_Project/myapp/src/components/Layout/Header.js
+++ 11_Practise_Project/myapp/src/components/Layout/Header.js
@@ -1,21 +1,21 @@
-import { Fragment } from 'react'
-import HeaderCartButton from "./HeaderCartButton"
-import classes from './Header.module.css'
-import mealsimage from '../../assets/meals.jpg'
+import { Fragment } from 'react';
 
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
-const Header = props => {
+const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
-                <img src={mealsimage}></img>
+                <img src={mealsImage} alt='A table full of delicious food!' />
             </div>
         </Fragment>
-    )
-}
+    );
+};
 
 export default Header;
